f820c592027423dd7ae7ec13f9bff39bc4fbe20d
mick3 <mvwebber3@gmail.com>
2020-11-17 20:40:49 +0000

    added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['mick3-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mick3-boutique-ado', 'mick3-boutique-ado.herokuapp.com/', 'localhost']
+
 
 
 # Application definition
@@ -175,6 +176,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    #Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
+
     # BUCKET CONFIG
     AWS_STORAGE_BUCKET_NAME = 'mick3-boutique-ado'
     AWS_S3_REGION_NAME = 'us-east-1'
